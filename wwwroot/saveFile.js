c13aa4e1605992e63e74d2687270b4386b7f659b
Jussi Virkkala <jussivirkkala@hotmail.com>
2023-12-09 19:23:27 +0200

    b1.2.0

--- wwwroot/saveFile.js
+++
@@ -1,8 +0,0 @@
-function saveFile(file, Content) {
-     var link = document.createElement('a');
-     link.download = file;
-     link.href = "data:text/plain;charset=utf-8," + encodeURIComponent(Content)
-     document.body.appendChild(link);
-     link.click();
-     document.body.removeChild(link);
- }
