43fae4cbcc62b5d93bb3f4a23e5c5cbfe4593b9a
Jussi Virkkala <jussivirkkala@hotmail.com>
2024-12-03 18:59:49 +0200

    multiple events

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: QJFIe3Vp */
+/* Manifest version: 6RnOWOmD */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
